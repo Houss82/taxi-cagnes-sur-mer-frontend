978c75d3c6da3b0633a6983d1f1dff0aef2e3c11
Houss82 <houssem1982mansour@gmail.com>
2026-04-10 14:22:46 +0100

    installation code clarity

--- app/layout.jsx
+++ app/layout.jsx
@@ -98,6 +98,16 @@ export default function RootLayout({ children }) {
           gtag('config', 'AW-17953621076');
         `}
       </Script>
+      {/* Microsoft Clarity */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "w9iuh25rii");
+        `}
+      </Script>
       <body
         className={`${plusJakartaSans.variable} font-sans antialiased`}
         suppressHydrationWarning
